23a1e2e1149bce9fccc2433767b341888f0f4de7
JAYADEEP G L <113873626+Jayadeep2004@users.noreply.github.com>
2023-12-19 19:29:30 +0530

    Create Self intro.java

---
+++ Self intro.java
@@ -0,0 +1,26 @@
+/* Author: JAYADEEP G L 
+  A Small self itroduction */
+
+public class Jayadeep{
+  public static void main(String[] args){
+    String name = "JAYADEEP G L ";
+    int age = 19;
+    String gender = "Male";
+    System.out.println("Self intro:");
+    System.out.println("NAME: " + name);
+    System.out.println("D.O.B: 21.01.2004");
+    System.out.println("Age: " + age);
+    System.out.println("Gender: " + gender);
+    System.out.println("Compelted schooling in GV higher secondary school");
+    System.out.println("Currently pursuing my UG degree at SRM University");
+  }
+}
+
+/*Output:
+Self intro:
+NAME: JAYADEEP G L 
+D.O.B: 21.01.2004
+Age: 19
+Gender: Male
+Compelted schooling in GV higher secondary school
+Currently pursuing my UG degree at SRM University */
