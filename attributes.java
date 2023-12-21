3c9268d418c103e3fbb0fed757e8953ed6afc685
JAYADEEP G L <113873626+Jayadeep2004@users.noreply.github.com>
2023-12-21 22:27:07 +0530

    Create attributes.java

---
+++ attributes.java
@@ -0,0 +1,18 @@
+/* Author : JAYADEEP G L 
+  attributes.java */
+
+public class Main {
+  String fname = "Jaya";
+  String lname = "Deep";
+  int age = 19;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: " + myObj.fname + " " + myObj.lname);
+    System.out.println("Age: " + myObj.age);
+  }
+}
+
+/* Output : 
+Name: Jaya Deep
+Age: 19 */
