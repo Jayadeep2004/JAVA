1ea965fad5d5a5d5522ef745290393a502a662d3
JAYADEEP G L <113873626+Jayadeep2004@users.noreply.github.com>
2023-12-21 22:37:22 +0530

    Create Class methods by passing multiple parameters.java

---
+++ Class methods by passing multiple parameters.java
@@ -0,0 +1,19 @@
+/* Author : JAYADEEP G L
+ Class methods by passing multiple parameters */
+
+public class Main {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + " is " + age);
+  }
+
+  public static void main(String[] args) {
+    myMethod("Liam", 5);
+    myMethod("Jenny", 8);
+    myMethod("Anja", 31);
+  }
+}
+
+/* Output:
+  Liam is 5
+  Jenny is 8
+  Anja is 31  */
